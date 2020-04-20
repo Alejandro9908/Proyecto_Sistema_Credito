5104bc1c156704927b24ed3f3d2957d640726e16
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-04-20 12:37:24 -0600

    Agregar librerias

--- src/controladores/Conexion.java
+++ src/controladores/Conexion.java
@@ -19,6 +19,7 @@ import java.util.logging.Logger;
  */
 public class Conexion {
     
+    //Hola como estan
     private static Connection conexion;
     private static String bdName = "DB_Creditos";
     private static String user = "Proyecto_Creditos";//Nombre del usuario
