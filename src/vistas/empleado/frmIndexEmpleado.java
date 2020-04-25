0aed28029a23744aa19b6dbfb256a8e381e2c38f
adria <adria@192.168.56.1>
2020-04-24 18:02:47 -0600

    Arreglo Reportes Empleados

--- src/vistas/empleado/frmIndexEmpleado.java
+++ src/vistas/empleado/frmIndexEmpleado.java
@@ -19,7 +19,6 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
-import vistas.departametoEmpresa.frmIndexDepatamentosEmpresa;
 import static vistas.frmEscritorio.dpnlEscritorio;
 
 /**
